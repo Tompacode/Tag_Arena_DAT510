3a95b98d56ec71fa531052e695e13c42727b7555
Linn Åstrand <157240744+linnastr@users.noreply.github.com>
2026-02-27 20:37:09 +0100

    Add load and swap characters

--- Assets/Scripts/GameMechanics/GameManager.cs
+++ Assets/Scripts/GameMechanics/GameManager.cs
@@ -4,8 +4,21 @@ using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
+    public string[] player1Team = new string[2];
+    public string[] player2Team = new string[2];
+
     private void Awake() //Hämta refernser osv
     {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // remove duplicates
+            return;
+        }
+
+        Instance = this;
         DontDestroyOnLoad(this.gameObject);
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,4 +32,27 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public void swapPlayer1()
+    {
+        string temp = player1Team[0];
+        player1Team[0] = player1Team[1];
+        player1Team[1] = temp;
+    }
+
+    public void swapPlayer2()
+    {
+        string temp = player2Team[0];
+        player2Team[0] = player2Team[1];
+        player2Team[1] = temp;
+    }
+
+    public void ClearTeams()
+    {
+        player1Team[0] = null;
+        player1Team[1] = null;
+        player2Team[0] = null;
+        player2Team[1] = null;
+    }
+
 }
